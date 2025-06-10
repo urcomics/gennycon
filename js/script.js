bb10c5364c6fa8acccd759fdc886d789b46eeb06
melissa <yaoduanmu3@gmail.com>
2025-06-10 15:09:25 -0600

    images pt 1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 $(function (){
+    //loading incs
     $("#navbar").load("../inc/nav.html");
+
+    //slideshow
+    // let slide_index = 0;
+    // slideshow();
+
+    // function slideshow(){
+    //     let i;
+    //     let slides = document.getElementsByClassName("slide_img");
+    //     for (i = 0; i < slides.length; i++) {
+    //         slides[i].style.display = "none";  
+    //     }
+    //     slide_index++;
+    //     if (slide_index > slides.length) {slide_index = 1}
+    //     slides[slide_index-1].style.display = "block"; 
+    //     setTimeout(slideshow, 2300);
+    // }
 });
 
 // function nightMode(){
