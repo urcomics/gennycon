0d53a32b83f38015dea708228c6abe75d579f9b7
melissa <yaoduanmu3@gmail.com>
2025-06-12 14:43:38 -0600

    self fonts

--- js/script.js
+++ js/script.js
@@ -20,14 +20,3 @@ $(function (){
     //     setTimeout(slideshow, 2300);
     // }
 });
-
-// function nightMode(){
-//     document.body.classList.toggle("nightMode");
-//     document.getElementById("navbar").classList.toggle("nightMode");
-
-//     if(document.getElementById("nightToggle").innerHTML.trim() === "☽"){ //if night
-//         document.getElementById("nightToggle").innerHTML = "&#9788;"; //switch to day
-//     } else {
-//         document.getElementById("nightToggle").innerHTML = "&#9789;"; //switch to night
-//     }
-// }
